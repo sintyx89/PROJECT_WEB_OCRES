f0a9cd05d7decb2a16bf11bf0e6d40df90426462
margaux <margaux.mengoni@gmail.com>
2021-11-27 13:39:45 +0100

    Project Start : Front Start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import User from './components/user';
+import Nav from './containers/nav';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      users : [
+        {
+          name: 'Margaux',
+          surname: 'MENGONI',
+          birthdate: '07/04/1998',
+          badge: '4'
+        },
+        {
+          name: 'Nicolas',
+          surname: 'LEBON',
+          birthdate: '26/05/2000',
+          badge: '5'
+        },
+      ],
+
+      selectedUser : 'Margaux',
+      selectedPokemon : 'Pikachu',
+      city : 'Paris'
+    }
+  }
+
+  changeUser(selectedUser) {
+    this.setState({ selectedUser });
+    // Revient a faire => this.setState({ selectedUser: selectedUser }); 
+  }
+
+   render() {
+    const { users, selectedUser } = this.state;
+
+    return (
+      <div class='App'>
+        <header class='App-header'>
+          <h1> Notre super POKEDEX version OCRES </h1>
+          <Nav users={users} changeUser={this.changeUser.bind(this)} />
+        </header> 
+        
+        <div class='mainContent'>
+          <div class='barreRecherche'>
+          <h2> Recherche </h2> 
+            <input 
+              type='text'
+              name='Pokemon'
+              value={this.selectedPokemon}
+            />
+          </div>
+
+          <div class='pokedexInfo'>
+            
+          </div>
+          
+          <div class='profilDresseur'>
+            <h2> Profil dresseur </h2>
+            <User user={users.find((user) => user.name === selectedUser)}/>
+          </div>
+          
+        </div>
+      </div>
+    );
+   }
+    
 }
 
-export default App;
+
