9c4fe39cff8c7ab0ee27bc2f3ec56b22420662ea
Nicolas <nicolas.lebon00@gmail.com>
2021-12-09 20:52:29 +0100

    recharts part5

--- frontend/src/components/pokemon.js
+++ frontend/src/components/pokemon.js
@@ -1,7 +1,11 @@
-import React from 'react';
 import '../index.css';
 
+import React, { PureComponent } from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+
+
 export default class Pokemon extends React.Component {
+
 
 
     render() {
@@ -16,6 +20,39 @@ export default class Pokemon extends React.Component {
         const weight = parseFloat(data.data.weight) / 10;
         const sprite = data.data.sprites.front_default;
         const type = data.data.types[0].type.name;
+        const hpstat = data.data.stats[0].base_stat;
+        const attstat = data.data.stats[1].base_stat;
+        const defstat = data.data.stats[2].base_stat;
+        const satstat = data.data.stats[3].base_stat;
+        const sdefstat = data.data.stats[4].base_stat;
+        const speedstat = data.data.stats[5].base_stat;
+        const info = [
+            {
+                subject: 'HP',
+                A: hpstat
+            },
+            {
+                subject: 'Attaque',
+                A: attstat
+            },
+            {
+                subject: 'Defense',
+                A: defstat
+            },
+            {
+                subject: 'Attaque Speciale',
+                A: satstat
+            },
+            {
+                subject: 'Defense Speciale',
+                A: sdefstat
+            },
+            {
+                subject: 'Vitesse',
+                A: speedstat
+            }
+        ];
+
 
         // affichage des infos
 
@@ -39,15 +76,33 @@ export default class Pokemon extends React.Component {
                                 {`Type : ${type}`}
                             </div>
                         </div>
-                       
+
                     </div>
                     <div class="col-auto">
-                            <div>
-                                <img
-                                    src={sprite}
-                                />
-                            </div>
+                        <div>
+                            <img
+                                src={sprite}
+                            />
                         </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <RadarChart
+                            outerRadius={100}
+                            width={440}
+                            height={250}
+                            data={info}
+                        >
+                            <PolarGrid stroke="#000"/>
+                            <PolarAngleAxis dataKey="subject" />
+                            <Radar
+                                name="Pokemon"
+                                dataKey="A"
+                                stroke="#160085"
+                                fill="#160085"
+                                fillOpacity={0.6}
+                            />
+                        </RadarChart>
+                    </div>
                 </div>
             </div>
 
