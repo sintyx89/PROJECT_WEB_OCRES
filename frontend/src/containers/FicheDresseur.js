c6a00f11788bad3b6e5dd4cacba7ce96a1dec9f3
margaux <margaux.mengoni@gmail.com>
2021-12-10 00:16:34 +0100

    fiche dresseur

---
+++ frontend/src/containers/FicheDresseur.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from "axios";
+import "../App.css";
+import '../index.css';
+
+const API_URL = "https://localhost:2000/api/dresseur";
+
+export default class FicheBerry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          dataPoke: null
+        };
+      }
+      callAPI = (id) => {
+        // Call API
+        axios
+          .get(`${API_URL}/${id}/`)
+          .then(({ data }) => {
+            console.log(data);
+            // Recupere uniquement la propriété data
+            const dataPoke = { data };
+            
+            this.setState({ dataPoke });
+          })
+          .catch(console.error);
+      };
+
+    // Lance un appel au lancement du component
+    componentDidMount() {
+        const { id } = this.props;
+        this.callAPI(id);
+    }
+
+    // A chaque update relance une api
+    componentDidUpdate(nextProps) {
+      // Ici on verifie que la mise à jour concerne bien le champs city
+      if (nextProps !== this.props.id) {
+        this.callAPI(nextProps.id);
+      }
+    }
+
+    render() {
+        const { dataPoke } = this.state;
+
+        if (!dataPoke) return <p>Loading...</p>;
+        
+        return (
+          <div>
+              
+          </div>
+        );
+      }
+}
