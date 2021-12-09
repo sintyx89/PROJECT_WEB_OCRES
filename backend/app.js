2e676290c1af12a2ff41fdc75bbbd7c84bb15edf
margaux <margaux.mengoni@gmail.com>
2021-12-09 19:41:09 +0100

    API Express : route GET

--- backend/app.js
+++ backend/app.js
@@ -16,4 +16,32 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+// Acces à l'API depuis n'importe quelle origine
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/stuff', (req, res, next) => {
+    const dresseur = [
+      {
+        _id: '1',
+        name: 'MENGONI',
+        first_name : 'Margaux',
+        birth_date : '07/04/1998',
+        nbr_badge : '4',
+      },
+      {
+        _id: '2',
+        name: 'LEBON',
+        first_name : 'Nicolas',
+        birth_date : '26/05/2000',
+        nbr_badge : '6',
+      },
+    ];
+    res.status(200).json(dresseur);
+  });
+
 module.exports = app;
